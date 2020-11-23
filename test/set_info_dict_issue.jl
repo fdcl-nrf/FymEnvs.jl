01793484ffabcaa3a784c88b34eae8e487901b21
JinraeKim <kjl950403@gmail.com>
2020-11-23 17:42:34 +0900

    Find a bug; set_info! cannot save Dict-in-Dict type data

---
+++ test/set_info_dict_issue.jl
@@ -0,0 +1,87 @@
+using FymEnvs
+using LinearAlgebra
+
+if !isdefined(Main, :TestEnvs)
+    # using Revise; includet("custom_env.jl")  # to avoid conflict
+    include("custom_env.jl")  # to avoid conflict
+    using .TestEnvs
+end
+
+
+function print_msg(test_name)
+    println(">"^6*" "*test_name*" "*"<"^6)
+end
+
+
+function test_custom_fym()
+    fym = TestEnv()
+
+    env = fym.env
+    log_dir = "data/test"
+    file_name = "custom.h5"
+    reset!(env)
+    _sample(fym, nothing, log_dir, file_name)
+    data, info = load(joinpath(log_dir, file_name),
+                      with_info=true)
+    _sample2(fym, nothing, log_dir, file_name)
+    data, info = load(joinpath(log_dir, file_name),
+                      with_info=true)
+end
+
+function _sample(fym::FymEnv, agent, log_dir, file_name)
+    env = fym.env
+    logger = Logger(log_dir=log_dir, file_name=file_name, max_len=1000)
+    obs = observe_flat(env)
+    i = 0
+    @time while true
+        if agent == nothing
+            action = nothing
+            if time(env.clock) > 0.5
+                fym.controller.K = - 3*Matrix(I, 3, 3)
+            end
+        end
+        next_obs, reward, done, info = env.step(action=action)
+        record(logger, info)
+        obs = next_obs
+        i += 1
+        if done
+            config = Dict(
+                          "name" => env.name,
+                       )
+            set_info!(logger, config)
+            break
+        end
+    end
+    close!(env)
+    close!(logger)
+end
+
+function _sample2(fym::FymEnv, agent, log_dir, file_name)
+    env = fym.env
+    logger = Logger(log_dir=log_dir, file_name=file_name, max_len=1000)
+    obs = observe_flat(env)
+    i = 0
+    @time while true
+        if agent == nothing
+            action = nothing
+            if time(env.clock) > 0.5
+                fym.controller.K = - 3*Matrix(I, 3, 3)
+            end
+        end
+        next_obs, reward, done, info = env.step(action=action)
+        record(logger, info)
+        obs = next_obs
+        i += 1
+        if done
+            config = Dict(
+                          "name" => Dict(),
+                       )
+            set_info!(logger, config)
+            break
+        end
+    end
+    close!(env)
+    close!(logger)
+end
+
+test_custom_fym()
